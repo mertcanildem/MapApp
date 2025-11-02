c1b86784d966e9c2ad1ddc8a40560b4d76e51290
mertildem <mertcanildem9@gmail.com>
2025-11-02 04:12:35 +0300

    Filled assets folder with new images and created Model named "Location" for our MVVM Architecture.

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,22 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Mert can Ildem on 2.11.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
+
+
