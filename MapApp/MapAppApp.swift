12b937b7cedd9a88ff9d1282739df76942462ead
mertildem <mertcanildem9@gmail.com>
2025-11-02 04:06:12 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Mert can Ildem on 2.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
