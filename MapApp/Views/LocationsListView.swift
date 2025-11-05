f718ba3e1751d2e491d23f403334b992e397cdff
mertildem <mertcanildem9@gmail.com>
2025-11-05 03:27:28 +0300

    Using a List as a custom animated menu #4

---
+++ MapApp/Views/LocationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsListView.swift
+//  MapApp
+//
+//  Created by Mert can Ildem on 4.11.2025.
+//
+
+import SwiftUI
+
+struct LocationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsListView()
+}
